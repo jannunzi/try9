5749c8c20ede2b4fbbb1e7b0be1d29a08cfe1067
jannunzi <jannunzi@gmail.com>
2020-10-27 19:05:14 -0400

    remove debugger

--- src/actions/applicationActions.js
+++ src/actions/applicationActions.js
@@ -2,14 +2,10 @@ import settingsService from "../services/settings.service.client"
 
 const FIND_ALL_SETTINGS = "FIND_ALL_SETTINGS"
 export const findAllSettings = (dispatch) =>
-{
-  debugger
   settingsService.findAllSettings()
     .then(settings => {
-      debugger
       dispatch({
         type: FIND_ALL_SETTINGS,
         settings
       })
     })
-}
