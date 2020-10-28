652ee56365d6c92f9772a79c068445c711a43cda
DESKTOP-H84EGGH\Jose Annunziato <jannunzi@gmail.com>
2020-10-28 11:33:13 -0400

    GUP-3634 Novina Configurator decryption of .AES file fails in Windows username has a space between the first and last name

--- public/server/services/openssl.service.server.js
+++ public/server/services/openssl.service.server.js
@@ -3,12 +3,20 @@ const utils = require('../common/utils')
 const openSslEncryptionCmd = (inputFile, outputFile) => `openssl aes-256-cbc    -salt -md md5 -k n0v1n@ -in ${inputFile} -out ${outputFile}`
 const openSslDecryptionCmd = (inputFile, outputFile) => `openssl aes-256-cbc -d -salt -md md5 -k n0v1n@ -in ${inputFile} -out ${outputFile}`
 
+const quotePathSpacesInWindows = (path) => process.platform === 'win32' ? `"${path}"` : path
+
 const openSslEncrypt = (inputFile, outputFile, callback) => {
+  inputFile = quotePathSpacesInWindows(inputFile)
+  outputFile = quotePathSpacesInWindows(outputFile)
+
   const CMD = openSslEncryptionCmd(inputFile, outputFile)
   utils.execShellCommandCallback(CMD, callback)
 }
 
 const openSslDecrypt = (inputFile, outputFile, callback) => {
+  inputFile = quotePathSpacesInWindows(inputFile)
+  outputFile = quotePathSpacesInWindows(outputFile)
+
   const CMD = openSslDecryptionCmd(inputFile, outputFile)
   utils.execShellCommandCallback(CMD, callback)
 }
